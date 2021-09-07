15083a19cac54c09405d1f485cfa24ec334ee961
ibidya <ibrayalcouye@gmail.com>
2021-09-07 11:52:14 +0300

    Initial Commit

---
+++ learnGH/learnGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnGHApp.swift
+//  learnGH
+//
+//  Created by MAC on 07/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct learnGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
